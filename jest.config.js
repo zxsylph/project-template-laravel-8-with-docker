5a56ae90c3ddc254cc87e0a1879503d6f37002f2
Wiroon Srisomburananont (WSYMBP13) <wiroon.sri@innosoft.kmutt.ac.th>
2020-12-18 09:33:18 +0700

    sampe jest with unit test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-	verbose: true,
+	verbose: false,
+	setupFilesAfterEnv: ['<rootDir>/__test__/setup.js'],
+	testMatch: ['<rootDir>/__test__/**/*.test.js'],
 	moduleNameMapper: {
 		'@components/(.*)': '<rootDir>/resources/js/components/$1'
 	}
